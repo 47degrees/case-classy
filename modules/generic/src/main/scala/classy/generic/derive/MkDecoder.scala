4cf5b713f322dbbf85bc8f8281ef1577190881fe
Edd Steel <edward.steel@gmail.com>
2017-10-24 20:23:16 -0700

    Fix examples in kebab-case comments

--- modules/generic/src/main/scala/classy/generic/derive/MkDecoder.scala
+++ modules/generic/src/main/scala/classy/generic/derive/MkDecoder.scala
@@ -310,19 +310,19 @@ object NamingStrategy {
   object LowerSnakeCase extends SplitJoinNamingStrategy(StringSplitter.split)(
     _.map(_.toLowerCase).mkString("_"))
 
-  /** A basic kebab case (`Kebab_case`) naming strategy that preserves
+  /** A basic kebab case (`Kebab-case`) naming strategy that preserves
     * character case
     */
   object KebabCase extends SplitJoinNamingStrategy(StringSplitter.split)(
     _.mkString("-"))
 
-  /** A basic kebab case (`KEBAB_CASE`) naming strategy that maps all
+  /** A basic kebab case (`KEBAB-CASE`) naming strategy that maps all
     * characters to uppercase
     */
   object UpperKebabCase extends SplitJoinNamingStrategy(StringSplitter.split)(
     _.map(_.toUpperCase).mkString("-"))
 
-  /** A basic kebab case (`kebab_case`) naming strategy that maps all
+  /** A basic kebab case (`kebab-case`) naming strategy that maps all
     * characters to lowercase
     */
   object LowerKebabCase extends SplitJoinNamingStrategy(StringSplitter.split)(
